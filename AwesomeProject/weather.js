0b3486b41a3d868c2b9c02994e9192b410d729d9
zhangyalin001 <zhangyalin@pin-dao.cn>
2020-12-15 21:14:23 +0800

    funs页面

--- AwesomeProject/weather.js
+++ AwesomeProject/weather.js
@@ -1,27 +1,60 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View }
-from 'react-native';
+import React, { Component } from "react";
+import { Image, StyleSheet, Text, View }
+	from "react-native";
 
 export default class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.instructions}>Hello ABC!</Text>
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.instructions}>
+					<Image source={{
+						uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+					}}
+					style={styles.imageIcon}/>
+					<Text style={styles.videoName}>
+            videoNamename
+					</Text>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5
-    }
+	container: {
+		margin:10,
+		backgroundColor:"#cccccc",
+		flex: 1,
+		// justifyContent: "center",
+		// alignItems: "center",
+		// backgroundColor: "#F5FCFF"
+	},
+	instructions: {
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#F5FCFF",
+		width: "50%",
+		// height:"40%",
+		// width: 300,
+		// height: 250,
+		borderWidth: 1,
+		borderColor: "#ccc",
+		borderStyle: "solid",
+		textAlign: "center",
+		color: "#333333",
+		// marginBottom: 5
+	},
+	imageIcon:{
+		// width: "50%",
+		// height: "50%",
+		width: "50%",
+		// **注意**，下面这个 height: undefined 样式，在加载 **本地图片** 时，是必须的！
+		height: undefined,
+		aspectRatio: 126 / 134,
+	},
+	borderImg:{
+
+		// width: 1,
+		// height: 300,
+	},
 });
